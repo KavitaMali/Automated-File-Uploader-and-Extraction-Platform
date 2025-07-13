576f922e050bbe4539acc3216654b548fcbd406e
kavitamali <malikavita388@gmail.com>
2025-07-13 14:30:43 +0530

    Add project files.

---
+++ AzureWebAPI/AzureBlobService.cs
@@ -0,0 +1,46 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+namespace AzureWebAPI
+{
+    public class AzureBlobService
+    {
+        BlobServiceClient _blobServiceClient;
+        BlobContainerClient _blobContainerClient;
+        string azureConnectionString = "DefaultEndpointsProtocol=https;AccountName=kbmstorage;AccountKey=OOtGXjZb/7SgpsIc3bLImNZ+OAoSn/31gUIMIHyEgRbj6WukMXu7ASk7YtMsUsiVAZOJ1xUwh+sE+AStWUm/gQ==;EndpointSuffix=core.windows.net";
+        public AzureBlobService()
+        {
+            _blobServiceClient = new BlobServiceClient(azureConnectionString);
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient("kbmstoragecontainer");
+        }
+
+        public async Task<List<BlobContentInfo>> UploadFiles(List<IFormFile> files)
+        {
+            var azureResponse = new List<BlobContentInfo>();
+            foreach (var file in files)
+            {
+                string fileName = file.FileName;
+                using (var memoryStream = new MemoryStream())
+                {
+                    file.CopyTo(memoryStream);
+                    memoryStream.Position = 0;
+                    var client = await _blobContainerClient.UploadBlobAsync(fileName, memoryStream, default);
+                    azureResponse.Add(client);
+                }
+            };
+            return azureResponse;
+        }
+        public async Task<List<BlobItem>> GetUploadedBlob()
+        {
+            var items = new List<BlobItem>();
+            var UploadedFiles = _blobContainerClient.GetBlobsAsync();
+            await foreach (BlobItem file in UploadedFiles)
+            {
+                items.Add(file);
+            }
+            return items;
+        }
+    }
+
+    
+}
+    
---
+++ AzureWebAPI/Controllers/AzureBlobController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AzureBlobController : ControllerBase
+    {
+        public AzureBlobService _service;
+        public AzureBlobController(AzureBlobService service)
+        {
+            _service = service;
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            var response= await _service.UploadFiles(files);
+            return Ok(response);
+        }
+
+        [HttpGet("BlobList")]
+        public async Task<IActionResult> GetBlobList()
+        {
+            var response = await _service.GetUploadedBlob();
+            return Ok(response);
+        }
+    }
+}
